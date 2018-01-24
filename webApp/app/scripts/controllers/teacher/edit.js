a46d99f5de01dd838ca93cb77187a81844536378
mengyunzhi <chuhang@yunzhiclub.com>
2018-01-24 15:24:59 +0800

    教师修改删除-前台

--- webApp/app/scripts/controllers/teacher/edit.js
+++ webApp/app/scripts/controllers/teacher/edit.js
@@ -8,11 +8,11 @@
  * Controller of the webApp
  */
 angular.module('webApp')
-  .controller('TeacherEditCtrl', function ($scope, $http, $stateParams) {
+  .controller('TeacherEditCtrl', function ($scope, $http, $stateParams, $state) {
     var self  = this;
 
     self.init = function () {
-      self.getTeacher();                                         // 调用方法获取数据
+      self.getTeacher();                                      // 调用方法获取数据
     };
 
     self.getTeacher = function () {
@@ -28,6 +28,20 @@ angular.module('webApp')
           console.log(response);                              // 失败, 打印控制台
         });
     };
+
+    self.submit = function () {
+      var id = $stateParams.id;                               // 获取id
+      var url = 'http://127.0.0.1:8080/Teacher/' + id;        // 定义url
+      $http.put(url, $scope.teacher)
+        .then(function success (response) {
+          console.log('success');
+          $state.go('teacher', {}, {reload: true});           // 成功，跳转到首页，刷新
+        }, function error (response) {
+          console.log('error' + url);                         // 失败，打印控制台
+        });
+    };
+
+    $scope.submit = self.submit;                              // 将submit赋值给$scope
 
     self.init();
   });
