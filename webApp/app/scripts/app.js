d2d8bb15cb988dc43d0dd1c249ee69b6d8cf3a6f
mengyunzhi <chuhang@yunzhiclub.com>
2018-01-24 16:11:46 +0800

    班级首页

--- webApp/app/scripts/app.js
+++ webApp/app/scripts/app.js
@@ -19,29 +19,37 @@ angular
   ])
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
+      // 教师管理
       .state({
-        name: 'teacher',                                    // 名称
+        name: 'teacher',                                    // 教师首页
         url: '/teacher',                                    // url
         controller: 'TeacherIndexCtrl',                     // 控制器名称
         templateUrl: 'views/teacher/index.html'             // V层地址
       })
       .state({
-        name: 'teacher.add',                                // 名称
+        name: 'teacher.add',                                // 教师添加
         url: '/add',                                        // url
         controller: 'TeacherAddCtrl',                       // 控制器名称
         templateUrl: 'views/teacher/add.html'               // V层地址
       })
       .state({
-        name: 'teacher.view',                               // 名称
+        name: 'teacher.view',                               // 教师查看
         url: '/view/:id',                                   // url, 带参数
         controller: 'TeacherViewCtrl',                      // 控制器名称
         templateUrl: 'views/teacher/view.html'              // V层地址
       })
       .state({
-        name: 'teacher.edit',                               // 名称
+        name: 'teacher.edit',                               // 教师编辑
         url: '/edit/:id',                                   // url, 带参数
         controller: 'TeacherEditCtrl',                      // 控制器名称
         templateUrl: 'views/teacher/edit.html'              // V层地址
+      })
+      // 班级管理 
+      .state({
+        name: 'klass',                                      // 班级首页
+        url: '/klass',                                      // url, 带参数
+        controller: 'KlassIndexCtrl',                       // 控制器名称
+        templateUrl: 'views/klass/index.html'               // V层地址
       });
     $urlRouterProvider
       .otherwise('/teacher');
---
+++ webApp/app/scripts/controllers/klass/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webApp.controller:KlassIndexCtrl
+ * @description
+ * # KlassIndexCtrl
+ * Controller of the webApp
+ */
+angular.module('webApp')
+  .controller('KlassIndexCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
