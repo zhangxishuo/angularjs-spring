62c0a7e8214eb506fc79bdcbd1d7f93c6af3edde
mengyunzhi <chuhang@yunzhiclub.com>
2018-01-25 12:49:40 +0800

    后台保存

--- api/src/main/java/com/mengyunzhi/SpringMVC/repository/KlassRepository.java
+++ api/src/main/java/com/mengyunzhi/SpringMVC/repository/KlassRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
  * 班级仓库接口
  * Created by zhangxishuo on 2018/1/25
  */
-public interface KlassRepository extends CrudRepository<Klass, Long> {
+public interface KlassRepository extends CrudRepository <Klass, Long> {
 }
--- api/src/main/java/com/mengyunzhi/SpringMVC/service/TeacherService.java
+++ api/src/main/java/com/mengyunzhi/SpringMVC/service/TeacherService.java
@@ -10,36 +10,14 @@ import java.util.List;
  */
 
 public interface TeacherService {
-    /**
-     * 获取所有教师
-     */
+
     List<Teacher> getAll();
 
-    /**
-     * 根据id获取教师
-     * @param id
-     * @return
-     */
     Teacher getOne(Long id);
 
-    /**
-     * 保存教师
-     * @param teacher
-     * @return
-     */
     Teacher save(Teacher teacher);
 
-    /**
-     * 更新教师
-     * @param id
-     * @param teacher
-     * @return
-     */
     Teacher update(Long id, Teacher teacher);
 
-    /**
-     * 删除教师
-     * @param id
-     */
     void delete(Long id);
 }
