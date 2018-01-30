ba00ac2757916a9c6d90e9b2963fb08d4241a6d9
zhangxishuo <zhangxishuo1998@gmail.com>
2018-01-30 23:11:43 +0800

    init teacher impl

---
+++ api/src/main/java/com/mengyunzhi/SpringMvcStudy/service/TeacherServiceImpl.java
@@ -0,0 +1,47 @@
+package com.mengyunzhi.SpringMvcStudy.service;
+
+import com.mengyunzhi.SpringMvcStudy.entity.Teacher;
+import com.mengyunzhi.SpringMvcStudy.repository.TeacherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * create by zhangxishuo
+ */
+@Service
+public class TeacherServiceImpl implements TeacherService {
+
+    @Autowired
+    TeacherRepository teacherRepository;              // 教师
+
+    @Override
+    public Iterable<Teacher> getAll() {
+        return teacherRepository.findAll();           // 调用findAll获取所有教师
+    }
+
+    @Override
+    public Teacher getOne(Long id) {
+        return teacherRepository.findOne(id);         // 调用findOne获取教师
+    }
+
+    @Override
+    public Teacher save(Teacher teacher) {
+        return teacherRepository.save(teacher);       // 调用save保存教师
+    }
+
+    @Override
+    public Teacher update(Long id, Teacher teacher) {
+        Teacher oldTeacher = teacherRepository.findOne(id);    // 获取教师
+        oldTeacher.setName(teacher.getName());
+        oldTeacher.setUsername(teacher.getUsername());
+        oldTeacher.setEmail(teacher.getEmail());
+        oldTeacher.setSex(teacher.isSex());           // 更新属性
+        return teacherRepository.save(teacher);       // 保存教师
+    }
+
+    @Override
+    public void delete(Long id) {
+        teacherRepository.delete(id);                 // 调用delete删除教师
+        return;
+    }
+}
