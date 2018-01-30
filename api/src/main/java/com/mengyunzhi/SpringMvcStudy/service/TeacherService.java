3857673b09e4c4f66de8e9450cec314d1e91639d
zhangxishuo <zhangxishuo1998@gmail.com>
2018-01-30 23:04:00 +0800

    init teacher interface

---
+++ api/src/main/java/com/mengyunzhi/SpringMvcStudy/service/TeacherService.java
@@ -0,0 +1,35 @@
+package com.mengyunzhi.SpringMvcStudy.service;
+
+import com.mengyunzhi.SpringMvcStudy.entity.Teacher;
+
+import java.util.List;
+
+/**
+ * create by zhangxishuo
+ */
+public interface TeacherService {
+    /**
+     * 获取教师列表
+     */
+    List<Teacher> getAll();
+
+    /**
+     * 根据id获取教师
+     */
+    Teacher getOne(Long id);
+
+    /**
+     * 保存教师
+     */
+    Teacher save(Teacher teacher);
+
+    /**
+     * 更新教师
+     */
+    Teacher update(Long id, Teacher teacher);
+
+    /**
+     * 根据id删除教师
+     */
+    void delete(Long id);
+}
