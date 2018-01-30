c003df1b10d21664d9057f393b40338a91b8fda8
zhangxishuo <zhangxishuo1998@gmail.com>
2018-01-30 22:55:49 +0800

    init teacher entity

---
+++ api/src/main/java/com/mengyunzhi/SpringMvcStudy/entity/Teacher.java
@@ -0,0 +1,65 @@
+package com.mengyunzhi.SpringMvcStudy.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * create by zhangxishuo
+ * 教师实体
+ */
+@Entity
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;                 // id
+
+    private String name;             // 姓名
+    private String username;         // 用户名
+    private String email;            // 邮箱
+    private boolean sex;             // 性别
+
+    public Teacher() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+}
