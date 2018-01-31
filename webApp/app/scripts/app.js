52fdc08d19fb83628e407aaf1a7cad01f1781228
zhangxishuo <zhangxishuo1998@gmail.com>
2018-01-31 19:25:50 +0800

    init route

--- webApp/app/scripts/app.js
+++ webApp/app/scripts/app.js
@@ -9,21 +9,57 @@
  * Main module of the application.
  */
 angular
-  .module('webApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'ui.router'
-  ])
-  .config(function ($stateProvider) {
-    $stateProvider
-      .state({
-        name: 'teacher',
-        url: '/teacher',
-        controller: 'TeacherIndexCtrl',
-        templateUrl: 'views/teacher/index.html'
-      });
-  });
+    .module('webApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'ui.router'
+    ])
+    .config(function($stateProvider, $urlRouterProvider, $provide, $httpProvider) {
+        $stateProvider
+            // 教师管理
+            .state({
+                name: 'teacher',
+                url: '/teacher',
+                controller: 'TeacherIndexCtrl',
+                templateUrl: 'views/teacher/index.html'
+            })
+            .state({
+                name: 'teacher.add',
+                url: '/add',
+                controller: 'TeacherAddCtrl',
+                templateUrl: 'views/teacher/add.html'
+            })
+            .state({
+                name: 'teacher.view',
+                url: '/view/:id',
+                controller: 'TeacherViewCtrl',
+                templateUrl: 'views/teacher/view.html'
+            })
+            .state({
+                name: 'teacher.edit',
+                url: '/edit/:id',
+                controller: 'TeacherEditCtrl',
+                templateUrl: 'views/teacher/edit.html'
+            });
+
+        $urlRouterProvider
+            .otherwise('/teacher');
+
+        $provide.factory('urlInterceptor', function() {
+            return {
+                'request': function(config) {
+                    var extension = config.url.split('.').pop();
+                    if (extension !== 'html') {
+                        config.url = 'http://127.0.0.1:8080' + config.url;
+                    }
+                    return config;
+                },
+            };
+        });
+
+        $httpProvider.interceptors.push('urlInterceptor');
+    });
