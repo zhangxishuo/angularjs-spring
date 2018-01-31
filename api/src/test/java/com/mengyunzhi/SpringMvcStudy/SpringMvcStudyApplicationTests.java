0bf6618968ed202d6c0ddad701be60576d335536
zhangxishuo <zhangxishuo1998@gmail.com>
2018-01-31 08:06:19 +0800

    revise unit test

--- api/src/test/java/com/mengyunzhi/SpringMvcStudy/SpringMvcStudyApplicationTests.java
+++ api/src/test/java/com/mengyunzhi/SpringMvcStudy/SpringMvcStudyApplicationTests.java
@@ -5,12 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.transaction.Transactional;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class SpringMvcStudyApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
-
 }
