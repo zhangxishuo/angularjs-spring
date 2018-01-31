7cc487662891bc24e4b04ed88f200016c874397f
zhangxishuo <zhangxishuo1998@gmail.com>
2018-01-31 17:07:36 +0800

    init klass repository interface

---
+++ api/src/main/java/com/mengyunzhi/SpringMvcStudy/repository/KlassRepository.java
@@ -0,0 +1,10 @@
+package com.mengyunzhi.SpringMvcStudy.repository;
+
+import com.mengyunzhi.SpringMvcStudy.entity.Klass;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * create by zhangxishuo
+ */
+public interface KlassRepository extends CrudRepository <Klass, Long> {
+}
