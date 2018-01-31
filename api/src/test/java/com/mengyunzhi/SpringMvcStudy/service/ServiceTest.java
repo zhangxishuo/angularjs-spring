df037c01bd2a86d488a3e69d7ec326b65f95b23d
zhangxishuo <zhangxishuo1998@gmail.com>
2018-01-31 08:08:28 +0800

    init service test

---
+++ api/src/test/java/com/mengyunzhi/SpringMvcStudy/service/ServiceTest.java
@@ -0,0 +1,16 @@
+package com.mengyunzhi.SpringMvcStudy.service;
+
+import com.mengyunzhi.SpringMvcStudy.SpringMvcStudyApplicationTests;
+import org.junit.Test;
+
+import javax.transaction.Transactional;
+
+/**
+ * create by zhangxishuo
+ */
+public class ServiceTest extends SpringMvcStudyApplicationTests {
+    @Test
+    public void test() {
+
+    }
+}
