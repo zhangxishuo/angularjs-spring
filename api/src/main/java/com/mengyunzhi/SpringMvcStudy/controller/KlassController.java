a1c695c5b0e4db80d37d1207b972d8a117f145c6
zhangxishuo <zhangxishuo1998@gmail.com>
2018-01-31 17:09:53 +0800

    init klass controller

---
+++ api/src/main/java/com/mengyunzhi/SpringMvcStudy/controller/KlassController.java
@@ -0,0 +1,43 @@
+package com.mengyunzhi.SpringMvcStudy.controller;
+
+import com.mengyunzhi.SpringMvcStudy.entity.Klass;
+import com.mengyunzhi.SpringMvcStudy.service.KlassService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * create by zhangxishuo
+ */
+@RestController
+@RequestMapping("/Klass")
+public class KlassController {
+
+    @Autowired
+    private KlassService klassService;         // 班级
+
+    @GetMapping("/")
+    public Iterable<Klass> getAll() {
+        return klassService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Klass getOne(@PathVariable Long id) {
+        return klassService.getOne(id);
+    }
+
+    @PostMapping("/")
+    public Klass save(@RequestBody Klass klass) {
+        return klassService.save(klass);
+    }
+
+    @PutMapping("/{id}")
+    public Klass update(@PathVariable Long id, @RequestBody Klass klass) {
+        return klassService.update(id, klass);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        klassService.delete(id);
+        return;
+    }
+}
