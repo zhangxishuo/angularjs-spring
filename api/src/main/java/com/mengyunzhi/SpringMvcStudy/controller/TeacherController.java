fda725dc51e3514f39ff3e5bf43fe4342f0c341b
zhangxishuo <zhangxishuo1998@gmail.com>
2018-01-31 08:07:07 +0800

    init controller

---
+++ api/src/main/java/com/mengyunzhi/SpringMvcStudy/controller/TeacherController.java
@@ -0,0 +1,12 @@
+package com.mengyunzhi.SpringMvcStudy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * create by zhangxishuo
+ */
+@RestController
+@RequestMapping("/Teacher")
+public class TeacherController {
+}
