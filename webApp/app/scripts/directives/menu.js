059b3180866d29f719c652fdacaa55119ae67b23
zhangxishuo <zhangxishuo1998@gmail.com>
2018-02-02 18:46:17 +0800

    menu directive

--- webApp/app/scripts/directives/menu.js
+++ webApp/app/scripts/directives/menu.js
@@ -7,22 +7,30 @@
  * # menu
  */
 angular.module('webApp')
-    .directive('menu', function($state) {
+    .directive('menu', function($state, routes) {
         return {
             templateUrl: 'views/directive/menu.html',
             restrict: 'E',
             link: function postLink($scope) {
                 var self = this;
 
-                self.isActive = function(name) {
-                    if ($state.current.name === name) {
-                        return true;
-                    } else {
-                        return false;
+                self.isActive = function(state) {
+                    var currentState = $state.$current;
+                    if (currentState.parent) {
+                        while (currentState.parent.parent !== null) {
+                            currentState = currentState.parent;
+                        }
+                        if (currentState.name === state.name) {
+                            return true;
+                        } else {
+                            return false;
+                        }
                     }
+
                 };
 
                 $scope.isActive = self.isActive;
+                $scope.routes = routes;
             }
         };
     });
