3f4c19fa98588c9b49555633d3fc21456d8d31b1
zhangxishuo <zhangxishuo1998@gmail.com>
2018-02-02 18:45:57 +0800

    revise route

--- webApp/app/scripts/app.js
+++ webApp/app/scripts/app.js
@@ -18,58 +18,86 @@ angular
         'ngTouch',
         'ui.router'
     ])
-    .config(function($stateProvider, $urlRouterProvider, $provide, $httpProvider) {
-        $stateProvider
-            // 教师管理
-            .state({
-                name: 'teacher',
-                url: '/teacher',
-                controller: 'TeacherIndexCtrl',
-                templateUrl: 'views/teacher/index.html'
-            })
-            .state({
-                name: 'teacher.add',
-                url: '/add',
-                controller: 'TeacherAddCtrl',
-                templateUrl: 'views/teacher/add.html'
-            })
-            .state({
-                name: 'teacher.view',
-                url: '/view/:id',
-                controller: 'TeacherViewCtrl',
-                templateUrl: 'views/teacher/view.html'
-            })
-            .state({
-                name: 'teacher.edit',
-                url: '/edit/:id',
-                controller: 'TeacherEditCtrl',
-                templateUrl: 'views/teacher/edit.html'
-            })
-            // 班级管理
-            .state({
-                name: 'klass',
-                url: '/klass',
-                controller: 'KlassIndexCtrl',
-                templateUrl: 'views/klass/index.html'
-            })
-            .state({
-                name: 'klass.add',
-                url: '/add',
-                controller: 'KlassAddCtrl',
-                templateUrl: 'views/klass/add.html'
-            })
-            .state({
-                name: 'klass.view',
-                url: '/view/:id',
-                controller: 'KlassViewCtrl',
-                templateUrl: 'views/klass/view.html'
-            })
-            .state({
-                name: 'klass.edit',
-                url: '/edit/:id',
-                controller: 'KlassEditCtrl',
-                templateUrl: 'views/klass/edit.html'
-            });
+    .config(function($provide) {
+        $provide.constant('routes', [{
+            name: 'teacher',
+            url: '/teacher',
+            controller: 'TeacherIndexCtrl',
+            templateUrl: 'views/teacher/index.html',
+            data: {
+                title: '教师管理',
+                show: true
+            }
+        }, {
+            name: 'teacher.add',
+            url: '/add',
+            controller: 'TeacherAddCtrl',
+            templateUrl: 'views/teacher/add.html',
+            data: {
+                title: '增加',
+                show: false
+            }
+        }, {
+            name: 'teacher.view',
+            url: '/view/:id',
+            controller: 'TeacherViewCtrl',
+            templateUrl: 'views/teacher/view.html',
+            data: {
+                title: '查看',
+                show: false
+            }
+        }, {
+            name: 'teacher.edit',
+            url: '/edit/:id',
+            controller: 'TeacherEditCtrl',
+            templateUrl: 'views/teacher/edit.html',
+            data: {
+                title: '编辑',
+                show: false
+            }
+        }, {
+            name: 'klass',
+            url: '/klass',
+            controller: 'KlassIndexCtrl',
+            templateUrl: 'views/klass/index.html',
+            data: {
+                title: '班级管理',
+                show: true
+            }
+        }, {
+            name: 'klass.add',
+            url: '/add',
+            controller: 'KlassAddCtrl',
+            templateUrl: 'views/klass/add.html',
+            data: {
+                title: '新增',
+                show: false
+            }
+        }, {
+            name: 'klass.view',
+            url: '/view/:id',
+            controller: 'KlassViewCtrl',
+            templateUrl: 'views/klass/view.html',
+            data: {
+                title: '查看',
+                show: false
+            }
+        }, {
+            name: 'klass.edit',
+            url: '/edit/:id',
+            controller: 'KlassEditCtrl',
+            templateUrl: 'views/klass/edit.html',
+            data: {
+                title: '编辑',
+                show: false
+            }
+        }]);
+    })
+    .config(function($stateProvider, $urlRouterProvider, $provide, $httpProvider, routes) {
+        angular.forEach(routes, function(value) {
+            $stateProvider
+                .state(value);
+        });
 
         $urlRouterProvider
             .otherwise('/teacher');
