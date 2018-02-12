0e5a95c70a0a46e89fac7b7d5b85fa2ff71fb551
zhangxishuo <zhangxishuo1998@gmail.com>
2018-02-12 14:35:28 +0800

    teacher edit ctrl comment

--- webApp/app/scripts/controllers/teacher/edit.js
+++ webApp/app/scripts/controllers/teacher/edit.js
@@ -10,22 +10,22 @@
 angular.module('webApp')
     .controller('TeacherEditCtrl', function($scope, $state, $stateParams, teacher) {
         var self = this;
-        var id   = $stateParams.id;
+        var id   = $stateParams.id;                              // 获取id
 
-        self.init = function() {
-            teacher.one(id, function(data) {
-                $scope.data = data;
+        self.init = function() {                                 // 初始化方法
+            teacher.one(id, function(data) {                     // 获取教师
+                $scope.data = data;                              // 传入视图
             });
         };
 
-        self.submit = function() {
-            teacher.update(id, $scope.data, function(data) {
-                console.log('success', data);
-                $state.go('teacher', {}, {reload: true});
+        self.submit = function() {                               // 提交方法
+            teacher.update(id, $scope.data, function(data) {     // 调用更新方法
+                console.log('success', data);                    // 控制台打印
+                $state.go('teacher', {}, {reload: true});        // 页面跳转
             });
         };
 
-        $scope.submit = self.submit;
+        $scope.submit = self.submit;                             // 传入视图
 
-        self.init();
+        self.init();                                             // 初始化
     });
