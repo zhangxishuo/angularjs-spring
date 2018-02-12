64ad8d23db83711f2a9cab9886c781fef2f1ab53
zhangxishuo <zhangxishuo1998@gmail.com>
2018-02-12 14:03:09 +0800

    service code rebuild

--- webApp/app/scripts/services/teacher.js
+++ webApp/app/scripts/services/teacher.js
@@ -1,18 +1,27 @@
 'use strict';
 
 /**
- * @ngdoc service
- * @name webApp.teacher
- * @description
- * # teacher
- * Service in the webApp.
+ * 
+ * 
  */
 angular.module('webApp')
     .service('teacher', function($http) {
         var self = this;
+        var url  = '/Teacher/';
 
-        self.getAllTeachers = function(callback) {
-            var url = '/Teacher/';
+        self.blank = function(callback) {
+            var data = {
+                name: '',
+                username: '',
+                email: '',
+                sex: true
+            };
+            if (callback) {
+                callback(data);
+            }
+        };
+
+        self.all = function(callback) {
             $http.get(url)
                 .then(function success(response) {
                     if (callback) {
@@ -23,7 +32,56 @@ angular.module('webApp')
                 });
         };
 
+        self.one = function(id, callback) {
+            $http.get(url + id)
+                .then(function success(response) {
+                    if (callback) {
+                        callback(response.data);
+                    }
+                }, function error() {
+                    console.log('error' + url);
+                });
+        };
+
+        self.save = function(data, callback) {
+            $http.post(url, data)
+                .then(function success(response) {
+                    if (callback) {
+                        callback(response.data);
+                    }
+                }, function error() {
+                    console.log('error' + url);
+                });
+        };
+
+        self.update = function(id, data, callback) {
+            $http.put(url + id, data)
+                .then(function success(response) {
+                    if (callback) {
+                        callback(response.data);
+                    }
+                }, function error() {
+                    console.log('error' + url);
+                });
+        };
+
+        self.delete = function(id, callback) {
+            $http.delete(url + id)
+                .then(function success() {
+                    if (callback) {
+                        callback();
+                    }
+                }, function error() {
+                    console.log('error' + url);
+                });
+        };
+
         return {
-            getAllTeachers: self.getAllTeachers
+            blank  : self.blank,
+            all    : self.all,
+            one    : self.one,
+            save   : self.save,
+            update : self.update,
+            delete : self.delete
         };
     });
