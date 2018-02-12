37ad818b01b3b0a2ff090dd75de58a556701c880
zhangxishuo <zhangxishuo1998@gmail.com>
2018-02-12 14:18:36 +0800

    servise add comment

--- webApp/app/scripts/services/teacher.js
+++ webApp/app/scripts/services/teacher.js
@@ -1,16 +1,20 @@
 'use strict';
 
 /**
- * 
- * 
+ * 教师服务
+ * service:teacher
+ * zhangxishuo
  */
 angular.module('webApp')
     .service('teacher', function($http) {
         var self = this;
-        var url  = '/Teacher/';
+        var url  = '/Teacher/';                                  // 定义url
 
+        /**
+         * 获取空教师
+         */
         self.blank = function(callback) {
-            var data = {
+            var data = {                                         // 定义空对象
                 name: '',
                 username: '',
                 email: '',
@@ -21,6 +25,9 @@ angular.module('webApp')
             }
         };
 
+        /**
+         * 获取所有教师
+         */
         self.all = function(callback) {
             $http.get(url)
                 .then(function success(response) {
@@ -32,6 +39,9 @@ angular.module('webApp')
                 });
         };
 
+        /**
+         * 根据id获取相应教师
+         */
         self.one = function(id, callback) {
             $http.get(url + id)
                 .then(function success(response) {
@@ -43,6 +53,9 @@ angular.module('webApp')
                 });
         };
 
+        /**
+         * 保存教师
+         */
         self.save = function(data, callback) {
             $http.post(url, data)
                 .then(function success(response) {
@@ -54,6 +67,9 @@ angular.module('webApp')
                 });
         };
 
+        /**
+         * 更新教师
+         */
         self.update = function(id, data, callback) {
             $http.put(url + id, data)
                 .then(function success(response) {
@@ -65,6 +81,9 @@ angular.module('webApp')
                 });
         };
 
+        /**
+         * 根据id删除教师
+         */
         self.delete = function(id, callback) {
             $http.delete(url + id)
                 .then(function success() {
