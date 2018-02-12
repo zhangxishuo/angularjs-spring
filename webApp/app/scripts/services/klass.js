eeba94e8b8e96b325e89c9936b6824d0b227208b
zhangxishuo <zhangxishuo1998@gmail.com>
2018-02-12 17:57:15 +0800

    add new service

---
+++ webApp/app/scripts/services/klass.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name webApp.klass
+ * @description
+ * # klass
+ * Service in the webApp.
+ */
+angular.module('webApp')
+    .service('klass', function() {
+        // AngularJS will instantiate a singleton by calling "new" on this function
+    });
