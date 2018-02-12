a75192aca4be2d822461b2a2810974236c27897b
zhangxishuo <zhangxishuo1998@gmail.com>
2018-02-12 14:31:10 +0800

    teacher view ctrl comment

--- webApp/app/scripts/controllers/teacher/view.js
+++ webApp/app/scripts/controllers/teacher/view.js
@@ -1,22 +1,20 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name webApp.controller:TeacherViewCtrl
- * @description
- * # TeacherViewCtrl
- * Controller of the webApp
+ * 教师查看控制器
+ * controller:TeacherViewCtrl
+ * zhangxishuo
  */
 angular.module('webApp')
     .controller('TeacherViewCtrl', function($scope, $stateParams, teacher) {
         var self = this;
-        var id   = $stateParams.id;
+        var id   = $stateParams.id;                              // 获取id
 
-        self.init = function() {
-            teacher.one(id, function(data) {
-                $scope.data = data;
+        self.init = function() {                                 // 初始化方法
+            teacher.one(id, function(data) {                     // 获取教师
+                $scope.data = data;                              // 传入视图
             });
         };
 
-        self.init();
+        self.init();                                             // 初始化
     });
