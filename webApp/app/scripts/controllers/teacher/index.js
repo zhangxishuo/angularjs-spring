1c6dc0ce32298c618904ec714d1115383e026cc6
zhangxishuo <zhangxishuo1998@gmail.com>
2018-02-12 14:30:47 +0800

    revise index comment

--- webApp/app/scripts/controllers/teacher/index.js
+++ webApp/app/scripts/controllers/teacher/index.js
@@ -9,14 +9,14 @@ angular.module('webApp')
     .controller('TeacherIndexCtrl', function($scope, teacher) {
         var self = this;
 
-        self.init = function() {
-            teacher.all(function(data) {
+        self.init = function() {                                 // 初始化方法
+            teacher.all(function(data) {                         // 获取所有教师
                 $scope.lists = data;                             // 传入视图
             });
         };
 
-        self.delete = function(list) {
-            teacher.delete(list.id, function() {
+        self.delete = function(list) {                           // 删除方法
+            teacher.delete(list.id, function() {                 // 调用删除方法
                 list._delete = true;                             // 已删除设置为true，隐藏该项
             });
         };
